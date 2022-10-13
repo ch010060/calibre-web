ff6e437009a9e3cf98818fab5837c2ce732d5722
Olivier <olivier.maire.jp@gmail.com>
2022-10-13 23:45:01 +0900

    bugfix

--- cps/web.py
+++ cps/web.py
@@ -18,7 +18,6 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from asyncio.windows_events import NULL
 import os
 import json
 import mimetypes
